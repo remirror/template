f50ba099e5bfb9c6c48d1f5555705f4ba5228ab4
ifiokjr <ifiokotung@gmail.com>
2021-02-01 14:59:11 +0000

    Deploying to gh-pages from  @ d77314ce118ca008210c92f1c3401713099b1df6 🚀

---
+++ 631037e5.39a728ee.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{82:function(o){o.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"blog","name":"blog","count":2,"permalink":"/blog/tags/blog"}')}}]);
